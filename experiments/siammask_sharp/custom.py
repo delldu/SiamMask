1f4fce73e3b74c25bcb0b3cd8b4c77912d8b0868
Dell Du <18588220928@163.com>
2021-06-22 14:03:29 +0800

    Update

--- experiments/siammask_sharp/custom.py
+++ experiments/siammask_sharp/custom.py
@@ -1,14 +1,176 @@
 from models.siammask_sharp import SiamMask
-# from models.features import MultiStageFeature
-# from models.rpn import RPN, DepthCorr
 from models.rpn import DepthCorr
-# from models.mask import Mask
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.load_helper import load_pretrain
-from resnet import resnet50
 import pdb
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
+        super(Bottleneck, self).__init__()
+        # (Pdb) a
+        # self = Bottleneck(
+        #   (conv1): Conv2d(64, 64, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        #   (bn1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        #   (conv2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        #   (bn2): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        #   (conv3): Conv2d(64, 256, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        #   (bn3): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        #   (relu): ReLU(inplace=True)
+        #   (downsample): Sequential(
+        #     (0): Conv2d(64, 256, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        #     (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        #   )
+        # )
+        # inplanes = 64
+        # planes = 64
+        # stride = 1
+        # downsample = Sequential(
+        #   (0): Conv2d(64, 256, kernel_size=(1, 1), stride=(1, 1), bias=False)
+        #   (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        # )
+        # dilation = 1
+
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        # padding = (2 - stride) + (dilation // 2 - 1)
+        padding = 2 - stride
+        assert stride==1 or dilation==1, "stride and dilation must have one equals to zero at least"
+        if dilation > 1:
+            padding = dilation
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                                padding=padding, bias=False, dilation=dilation)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        # if out.size() != residual.size():
+        #     print(out.size(), residual.size())
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, layer4=False, layer3=False):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        # (Pdb) a
+        # self = ResNet()
+        # block = <class 'resnet.Bottleneck'>
+        # layers = [3, 4, 6, 3]
+        # layer4 = False
+        # layer3 = True
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=0, # 3
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2) # 31x31, 15x15
+
+        self.feature_size = 128 * block.expansion
+
+        if layer3:
+            self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2) # 15x15, 7x7
+            self.feature_size = (256 + 128) * block.expansion
+        else:
+            self.layer3 = lambda x:x # identity
+
+        if layer4:
+            self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4) # 7x7, 3x3
+            self.feature_size = 512 * block.expansion
+        else:
+            self.layer4 = lambda x:x  # identity
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        dd = dilation
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            if stride == 1 and dilation == 1:
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(planes * block.expansion),
+                )
+            else:
+                if dilation > 1:
+                    dd = dilation // 2
+                    padding = dd
+                else:
+                    dd = 1
+                    padding = 0
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                              kernel_size=3, stride=stride, bias=False,
+                              padding=padding, dilation=dd),
+                    nn.BatchNorm2d(planes * block.expansion),
+                )
+
+        layers = []
+        # layers.append(block(self.inplanes, planes, stride, downsample, dilation=dilation))
+        layers.append(block(self.inplanes, planes, stride, downsample, dilation=dd))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        p0 = self.relu(x)
+        x = self.maxpool(p0)
+
+        p1 = self.layer1(x)
+        p2 = self.layer2(p1)
+        p3 = self.layer3(p2)
+
+        return p0, p1, p2, p3
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+    return model
+
 
 class ResDownS(nn.Module):
     def __init__(self, inplane, outplane):
@@ -32,8 +194,8 @@ class ResDown(nn.Module):
     def __init__(self, pretrain=False):
         super(ResDown, self).__init__()
         self.features = resnet50(layer3=True, layer4=False)
-        if pretrain:
-            load_pretrain(self.features, 'resnet.model')
+        # if pretrain:
+        #     load_pretrain(self.features, 'resnet.model')
 
         self.downsample = ResDownS(1024, 256)
 
@@ -46,13 +208,13 @@ class ResDown(nn.Module):
         p3 = self.downsample(output[-1])
         # (Pdb) p3.size() -- torch.Size([1, 256, 7, 7])
 
-        return p3
-
-    def forward_all(self, x):
-        # (Pdb) x.size() -- torch.Size([1, 3, 255, 255])
-        output = self.features(x)
-        p3 = self.downsample(output[-1])
         return output, p3
+
+    # def forward_all(self, x):
+    #     # (Pdb) x.size() -- torch.Size([1, 3, 255, 255])
+    #     output = self.features(x)
+    #     p3 = self.downsample(output[-1])
+    #     return output, p3
 
 
 class UP(nn.Module):
@@ -167,11 +329,11 @@ class SiameseTracker(SiamMask):
 
     def template(self, template):
         # (Pdb) template.size() -- torch.Size([1, 3, 127, 127])
-        self.zf = self.features(template)
+        _, self.zf = self.features(template)
 
     def track_mask(self, search):
         # (Pdb) search.size() -- torch.Size([1, 3, 255, 255])
-        self.feature, self.search = self.features.forward_all(search)
+        self.feature, self.search = self.features(search)
         # (Pdb) self.zf.size() -- torch.Size([1, 256, 7, 7])
         rpn_pred_cls, rpn_pred_loc = self.rpn(self.zf, self.search)
         self.corr_feature = self.mask_model.mask.forward_corr(self.zf, self.search)
